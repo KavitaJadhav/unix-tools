18782234f8cd275baf45801cf16d9f6c8fc488ad
kavitama <jadhavkavita206@gmail.com>
2014-01-11 11:17:58 +0530

    default sort completed

--- src/kavitama/unixtools/Sort.java
+++ src/kavitama/unixtools/Sort.java
@@ -1,4 +1,22 @@
 package kavitama.unixtools;
+import java.util.Arrays;
 
 public class Sort {
+    public String joinArray(String[] content , String separator){
+        String result = "";
+        for (int i = 0; i <content.length ; i++) {
+            result += content[i] + separator;
+        }
+        return result;
+    }
+    public String defaultSort(String data) {
+        String[] lines = data.split("\n");
+        Arrays.sort(lines);
+        return joinArray(lines ,"\n");
+    }
+
+    public static void main(String[] args) {
+        Sort sort = new Sort();
+        System.out.println(sort.defaultSort("aaaaa\njjjjjj\nhhhhhhhh\nbbbbbbbb"));
+    }
 }
