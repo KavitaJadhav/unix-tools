2b3a14017860c3ed52b43c694121ccb81debf166
kavitama <jadhavkavita206@gmail.com>
2014-01-13 11:02:18 +0530

    test case for RemoveSpace added

--- test/kavitama/unixtools/RemoveSpacesTest.java
+++ test/kavitama/unixtools/RemoveSpacesTest.java
@@ -11,7 +11,7 @@ public class RemoveSpacesTest extends RemoveSpaces {
     @Test
     public void testRemoveBlankSpace() throws Exception {
         RemoveSpaces rs = new RemoveSpaces();
-        String content = "Hi      \nhow      r u \nwhats       going on\nkkk     jjj \nkkk jjjj";
+        String content = "Hi     \nhow      r u \nwhats       going on\nkkk     jjj \nkkk jjjj";
         String expected = "Hi \nhow r u \nwhats going on\nkkk jjj \nkkk jjjj";
 
         String actual = rs.removeBlankSpace(content);
