4492c3df2c632b5c9c24189a3ab38ff1a6f1f581
kavitama <jadhavkavita206@gmail.com>
2014-01-13 10:40:09 +0530

    test case for Cutl added

--- test/kavitama/unixtools/CutTest.java
+++ test/kavitama/unixtools/CutTest.java
@@ -2,12 +2,31 @@ package kavitama.unixtools;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by kavitama on 1/13/14.
  */
 public class CutTest extends Cut {
     @Test
     public void testGetSpecificFeild() throws Exception {
+        Cut cut = new Cut();
+        String content = "Hi\nhow r u \nwhats going on\nkkk jjj \nkkk jjjj";
+        String expected = "Hi\nhow\nwhats\nkkk\nkkk";
 
+        String actual = cut.getSpecificFeild(1,content," ");
+
+        assertEquals(expected , actual);
     }
+    @Test
+    public void testGetSpecificFeild2() throws Exception {
+        Cut cut = new Cut();
+        String content = "Hi\nhow r u \nwhats going on\nkkk jjj \nkkk jjjj";
+        String expected = "\nu\non\n\n";
+
+        String actual = cut.getSpecificFeild(3,content," ");
+
+        assertEquals(expected , actual);
+    }
+
 }
