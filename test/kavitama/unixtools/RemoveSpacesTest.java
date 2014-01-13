4ac760d40de071f87a72afd80663e8f137546468
kavitama <jadhavkavita206@gmail.com>
2014-01-13 11:46:22 +0530

    test case for defalt sort and default reverse sort added

--- test/kavitama/unixtools/RemoveSpacesTest.java
+++ test/kavitama/unixtools/RemoveSpacesTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by kavitama on 1/13/14.
- */
 public class RemoveSpacesTest extends RemoveSpaces {
     @Test
     public void testRemoveBlankSpace() throws Exception {
---
+++ test/kavitama/unixtools/SortTest.java
@@ -0,0 +1,30 @@
+package kavitama.unixtools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by kavitama on 1/13/14.
+ */
+public class SortTest extends Sort {
+    @Test
+    public void testDefaultSort() throws Exception {
+        String content ="aaaaa\njjjjjj\nhhhhhhhh\nbbbbbbbb";
+        String expected ="aaaaa\nbbbbbbbb\nhhhhhhhh\njjjjjj";
+
+        String actual = defaultSort(content);
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    public void testDefaultReverseSort() throws Exception {
+        String content ="aaaaa\njjjjjj\nhhhhhhhh\nbbbbbbbb";
+        String expected ="jjjjjj\nhhhhhhhh\nbbbbbbbb\naaaaa";
+
+        String actual = defaultReverseSort(content);
+
+        assertEquals(expected , actual);
+    }
+}
