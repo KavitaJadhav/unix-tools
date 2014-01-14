a93f9c3a8f54713c854f2ebec1ceac53f940ac01
kavitama <jadhavkavita206@gmail.com>
2014-01-14 17:17:02 +0530

    main function of RemoveSpace  is separated in RemoveSpaceClient class

--- src/client/removeSpaceClient.java
+++ src/client/removeSpaceClient.java
@@ -1,7 +1,12 @@
 package client;
 
-/**
- * Created by kavitama on 1/14/14.
- */
+import kavitama.unixtools.RemoveSpaces;
+
 public class removeSpaceClient {
+    public static void main(String args[]) {
+        RemoveSpaces rs = new RemoveSpaces();
+        String content = rs.readFile(args[0]);
+        content = rs.removeBlankSpace(content);
+        rs.writeFile(content,args[1]);
+    }
 }
--- src/kavitama/unixtools/RemoveSpaces.java
+++ src/kavitama/unixtools/RemoveSpaces.java
@@ -5,11 +5,4 @@ public class RemoveSpaces extends ReadWriteFile {
     public String removeBlankSpace(String content) {
         return content.replaceAll("[ ]+"," ");
     }
-
-    public static void main(String args[]) {
-        RemoveSpaces rs = new RemoveSpaces();
-        String content = rs.readFile(args[0]);
-        content = rs.removeBlankSpace(content);
-        rs.writeFile(content,args[1]);
-    }
 }
