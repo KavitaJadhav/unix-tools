cf44c8d8036e69d5b6df07146b645be60421963c
kavitama <jadhavkavita206@gmail.com>
2014-01-15 23:32:27 +0530

    Test case for default 10 line of Tail  is added

--- test/kavitama/unixtools/TailTest.java
+++ test/kavitama/unixtools/TailTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class TailTest extends Tail {
 
     @Test
-    public void testgetTail() throws Exception {
+    public void getGivenNumberofLineAtTail() throws Exception {
         Tail tail = new Tail();
         String content = "Hi\nhow r u \nwhats going on\nkkk jjj \nkkk jjjj";
         String expected = "whats going on\nkkk jjj \nkkk jjjj";
@@ -17,7 +17,7 @@ public class TailTest extends Tail {
         assertEquals(expected , actual);
     }
     @Test
-    public void testPrintHead2() throws Exception {
+    public void getAllLinesIfLinesLessThan10() throws Exception {
         Tail tail = new Tail();
         String content = "Hi\nhow r u \nwhats going on\nkkk jjj \nkkk jjjj";
         String expected = "Hi\nhow r u \nwhats going on\nkkk jjj \nkkk jjjj";
@@ -26,6 +26,14 @@ public class TailTest extends Tail {
 
         assertEquals(expected , actual);
     }
+    @Test
+    public void get10LinesAtTail() throws Exception {
+        Tail tail = new Tail();
+        String content = "Hi\nhow \nr u \nwhats \ngoing \non\nkkk \njjj \nkkk \njjjj\nWhats";
+        String expected = "how \nr u \nwhats \ngoing \non\nkkk \njjj \nkkk \njjjj\nWhats";
 
+        String actual = tail.getTail(content,10);
 
+        assertEquals(expected , actual);
+    }
 }
